377b9e4c0e69ab61534a304961ddbe1b06f91e70
Hitesh Maidasani <hmaidasani@gmail.com>
2013-12-01 23:26:11 -0500

    adding map data request

--- routes/getData.js
+++ routes/getData.js
@@ -64,15 +64,25 @@ var monthColumns = [
 ];
 
 exports.getData = function(req, res){
-	var output = {};
 	var type = req.body.type || req.query.type;
+	if(type == 'stores') {
+		sendStoresData(req, res);
+	} else if(type == 'map') {
+		sendMapData(req, res);
+	} else {
+		res.json({});
+	}
+};
+
+
+function sendStoresData(req, res) {
+	var output = {};
 	var name = req.body.name || req.query.name;
 	var timeType = req.body.timeType || req.query.timeType;
 	var time1 = req.body.time1 || req.query.time1;
 	var time2 = req.body.time2 || req.query.time2;
 	var value = req.body.value || req.query.value;
-	if(type == 'stores') {
-		switch(timeType)
+	switch(timeType)
 		{
 			case "hour":
 				output["columns"] = hourColumns;
@@ -164,7 +174,107 @@ exports.getData = function(req, res){
 			    
 			  });
 		});
-	} else {
-		res.json({});
-	}
-};
+}
+
+function sendMapData(req, res) {
+	var output = {};
+	var name = req.body.name || req.query.name;
+	var timeType = req.body.timeType || req.query.timeType;
+	var time1 = req.body.time1 || req.query.time1;
+	var time2 = req.body.time2 || req.query.time2;
+	var value = req.body.value || req.query.value;
+	switch(timeType)
+		{
+			case "hour":
+				output["columns"] = hourColumns;
+				break;
+			case "day":
+				output["columns"] = dayColumns;
+				break;
+			case "month":
+				output["columns"] = monthColumns;
+				break;
+			default:
+				output["columns"] = "timeType parameter not specified";
+		}
+		
+		var queryString = 'SELECT name as store, id FROM stores';
+		if(name == 'all' || name === undefined)
+			queryString += '';
+		else
+			queryString += ' WHERE LOWER(name) like \'%' + name.toLowerCase() +'%\'';
+		
+		var stores = {};
+		var pg = require('pg'); 
+		pg.connect(process.env.DATABASE_URL || 'postgres://localhost:5432/floorplanviz', function(err, client, done) {
+			client.query(queryString, function(err, result) {
+			    done();
+			    if(err) return console.error(err);
+			    
+			    output["rows"] = result.rows;
+			    for(var i in result.rows) {
+			    	stores[result.rows[i].store] = i*1+1;
+			    }
+			    
+			    if(value == 'customers' && (time1 || time2)) {
+			    	var time1Moment = moment(time1);
+			    	var time2Moment = moment(time2);
+			    	var time = time1Moment.year()+'-'
+			    	if(time1 && time2)
+			    		queryString = "select round(avg(number),2) as value, store_id as id from storecustomers inner join stores on storecustomers.store_id = stores.id where time >= \'" + time1Moment.format("YYYY-MM-DD HH:00") +"\' and time <= \'" + time2Moment.format("YYYY-MM-DD HH:00") +"\'";
+			    	else if(time1)
+			    		queryString = "select round(avg(number),2) as value, store_id as id from storecustomers inner join stores on storecustomers.store_id = stores.id where time = \'" + time1Moment.format("YYYY-MM-DD HH:00") +"\'";
+			    	else if(time1)
+			    		queryString = "select round(avg(number),2) as value, store_id as id from storecustomers inner join stores on storecustomers.store_id = stores.id where time = \'" + time2Moment.format("YYYY-MM-DD HH:00") +"\'";
+			    	if(name == 'all' || name === undefined)
+			    		queryString += '';
+			    	else
+			    		queryString += ' AND LOWER(name) like \'%' + name.toLowerCase() +'%\'';
+			    	queryString += ' group by store_id order by store_id';
+			    	console.log(queryString);
+			    	client.query(queryString, function(err, result) {
+			    		done();
+			    		if(err) return console.error(err);
+			    		var results = [];
+			    		for(var i in result.rows) {
+			    			results.push({"id":result.rows[i].id, "value":result.rows[i].value});
+			    		}
+			    		output["values"] = results;
+
+			    		res.json(output);
+			    	});
+			    } else if(value == 'profit' && (time1 || time2)) {
+			    	var time1Moment = moment(time1);
+			    	var time2Moment = moment(time2);
+			    	var time = time1Moment.year()+'-'
+			    	if(time1 && time2)
+			    		queryString = "select round(avg(profit),2) as value, store_id as id from storeprofits inner join stores on storeprofits.store_id = stores.id where time >= \'" + time1Moment.format("YYYY-MM-DD HH:00") +"\' and time <= \'" + time2Moment.format("YYYY-MM-DD HH:00") +"\'";
+			    	else if(time1)
+			    		queryString = "select round(avg(profit),2) as value, store_id as id from storeprofits inner join stores on storeprofits.store_id = stores.id where time = \'" + time1Moment.format("YYYY-MM-DD HH:00") +"\'";
+			    	else if(time1)
+			    		queryString = "select round(avg(profit),2) as value, store_id as id from storeprofits inner join stores on storeprofits.store_id = stores.id where time = \'" + time2Moment.format("YYYY-MM-DD HH:00") +"\'";
+			    	if(name == 'all' || name === undefined)
+			    		queryString += '';
+			    	else
+			    		queryString += ' AND LOWER(name) like \'%' + name.toLowerCase() +'%\'';
+			    	queryString += ' group by store_id order by store_id';
+			    	console.log(queryString);
+			    	client.query(queryString, function(err, result) {
+			    		done();
+			    		if(err) return console.error(err);
+			    		var results = [];
+			    		for(var i in result.rows) {
+			    			results.push({"id":result.rows[i].id, "value":result.rows[i].value});
+			    		}
+			    		output["values"] = results;
+
+			    		res.json(output);
+			    	});
+			    } else {
+			    	output["values"] = "provide time time1 or time2 parameters";
+			    	res.json(output);
+			    }
+			    
+			  });
+		});
+}
