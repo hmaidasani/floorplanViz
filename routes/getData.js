cd67d7ac37de186009147541cadfdd91fff8d5be
Hitesh Maidasani <hmaidasani@gmail.com>
2013-12-05 19:07:18 -0500

    adding paths request

--- routes/getData.js
+++ routes/getData.js
@@ -5,11 +5,13 @@
 
 var moment = require('moment');
 
+
+
 var hourColumns = [
     {"time":"9a"},
     {"time":"10a"},
     {"time":"11a"},
-    {"time":"12a"},
+    {"time":"12p"},
     {"time":"1p"},
     {"time":"2p"},
     {"time":"3p"},
@@ -77,8 +79,10 @@ exports.getData = function(req, res){
 	var type = req.body.type || req.query.type;
 	if(type == 'stores') {
 		sendStoresData(req, res);
+	} else if (type == 'paths') {
+		sendPathData(req, res);
 	} else if(type == 'map') {
-		sendMapData(req, res);
+		// sendMapData(req, res);
 	} else {
 		res.json({});
 	}
@@ -198,6 +202,8 @@ function sendStoresData(req, res) {
 	});
 }
 
+
+
 function binData(rows, timeType, stores) {
 	var storeToTimeBins = {};
 	switch(timeType)
@@ -249,105 +255,59 @@ function binData(rows, timeType, stores) {
 	return storeToTimeBins;
 }
 
-// function sendMapData(req, res) {
-// 	var output = {};
-// 	var name = req.body.name || req.query.name;
-// 	var timeType = req.body.timeType || req.query.timeType;
-// 	var time1 = req.body.time1 || req.query.time1;
-// 	var time2 = req.body.time2 || req.query.time2;
-// 	var value = req.body.value || req.query.value;
-// 	switch(timeType)
-// 	{
-// 		case "hour":
-// 			output["columns"] = hourColumns;
-// 			break;
-// 		case "day":
-// 			output["columns"] = dayColumns;
-// 			break;
-// 		case "month":
-// 			output["columns"] = monthColumns;
-// 			break;
-// 		default:
-// 			output["columns"] = "timeType parameter not specified";
-// 	}
-	
-// 	var queryString = 'SELECT name as store, id FROM stores';
-// 	if(name == 'all' || name === undefined)
-// 		queryString += '';
-// 	else
-// 		queryString += ' WHERE LOWER(name) like \'%' + name.toLowerCase() +'%\'';
-	
-// 	var stores = {};
-// 	var pg = require('pg'); 
-// 	pg.connect(process.env.DATABASE_URL || 'postgres://localhost:5432/floorplanviz', function(err, client, done) {
-// 		client.query(queryString, function(err, result) {
-// 		    done();
-// 		    if(err) return console.error(err);
-		    
-// 		    output["rows"] = result.rows;
-// 		    for(var i in result.rows) {
-// 		    	stores[result.rows[i].store] = i*1+1;
-// 		    }
-		    
-// 		    if(value == 'customers' && (time1 || time2)) {
-// 		    	var time1Moment = moment(time1);
-// 		    	var time2Moment = moment(time2);
-// 		    	var time = time1Moment.year()+'-'
-// 		    	if(time1 && time2)
-// 		    		queryString = "select round(avg(number),2) as value, store_id as id from storecustomers inner join stores on storecustomers.store_id = stores.id where time >= \'" + time1Moment.format("YYYY-MM-DD HH:00") +"\' and time <= \'" + time2Moment.format("YYYY-MM-DD HH:00") +"\'";
-// 		    	else if(time1)
-// 		    		queryString = "select round(avg(number),2) as value, store_id as id from storecustomers inner join stores on storecustomers.store_id = stores.id where time = \'" + time1Moment.format("YYYY-MM-DD HH:00") +"\'";
-// 		    	else if(time1)
-// 		    		queryString = "select round(avg(number),2) as value, store_id as id from storecustomers inner join stores on storecustomers.store_id = stores.id where time = \'" + time2Moment.format("YYYY-MM-DD HH:00") +"\'";
-// 		    	if(name == 'all' || name === undefined)
-// 		    		queryString += '';
-// 		    	else
-// 		    		queryString += ' AND LOWER(name) like \'%' + name.toLowerCase() +'%\'';
-// 		    	queryString += ' group by store_id order by store_id';
-// 		    	console.log(queryString);
-// 		    	client.query(queryString, function(err, result) {
-// 		    		done();
-// 		    		if(err) return console.error(err);
-// 		    		var results = [];
-// 		    		for(var i in result.rows) {
-// 		    			results.push({"id":result.rows[i].id, "value":result.rows[i].value});
-// 		    		}
-// 		    		output["values"] = results;
 
-// 		    		res.json(output);
-// 		    	});
-// 		    } else if(value == 'profit' && (time1 || time2)) {
-// 		    	var time1Moment = moment(time1);
-// 		    	var time2Moment = moment(time2);
-// 		    	var time = time1Moment.year()+'-'
-// 		    	if(time1 && time2)
-// 		    		queryString = "select round(avg(profit),2) as value, store_id as id from storeprofits inner join stores on storeprofits.store_id = stores.id where time >= \'" + time1Moment.format("YYYY-MM-DD HH:00") +"\' and time <= \'" + time2Moment.format("YYYY-MM-DD HH:00") +"\'";
-// 		    	else if(time1)
-// 		    		queryString = "select round(avg(profit),2) as value, store_id as id from storeprofits inner join stores on storeprofits.store_id = stores.id where time = \'" + time1Moment.format("YYYY-MM-DD HH:00") +"\'";
-// 		    	else if(time1)
-// 		    		queryString = "select round(avg(profit),2) as value, store_id as id from storeprofits inner join stores on storeprofits.store_id = stores.id where time = \'" + time2Moment.format("YYYY-MM-DD HH:00") +"\'";
-// 		    	if(name == 'all' || name === undefined)
-// 		    		queryString += '';
-// 		    	else
-// 		    		queryString += ' AND LOWER(name) like \'%' + name.toLowerCase() +'%\'';
-// 		    	queryString += ' group by store_id order by store_id';
-// 		    	console.log(queryString);
-// 		    	client.query(queryString, function(err, result) {
-// 		    		done();
-// 		    		if(err) return console.error(err);
-// 		    		var results = [];
-// 		    		for(var i in result.rows) {
-// 		    			results.push({"id":result.rows[i].id, "value":result.rows[i].value});
-// 		    		}
-// 		    		output["values"] = results;
+function sendPathData(req, res) {
+	// select * from shoppingdurations where hour between 15 and 20 and day between 4 and 7 and month between 4 and 4;
+	var hour1 = req.body.hour1 || req.query.hour1;
+	var hour2 = req.body.hour2 || req.query.hour2;
+	var day1 = req.body.day1 || req.query.day1;
+	var day2 = req.body.day2 || req.query.day2;
+	var month1 = req.body.month1 || req.query.month1;
+	var month2 = req.body.month2 || req.query.month2;
 
-// 		    		res.json(output);
-// 		    	});
-// 		    } else {
-// 		    	output["values"] = "provide time time1 or time2 parameters";
-// 		    	res.json(output);
-// 		    }
+	if(!hour1 || !hour2 || !day1 || !day2 || !month1 || !month2) res.json({});
+	
+	var queryString = 'SELECT * FROM shoppingdurations where';
+	queryString += ' hour between ' + hour1 + ' and ' + hour2;
+	queryString += ' AND day between ' + day1 + ' and ' + day2;
+	queryString += ' AND month between ' + month1 + ' and ' + month2;
+	console.log(queryString);
+	var storePaths = {};
+	var output = [];
+	var pg = require('pg'); 
+	pg.connect(process.env.DATABASE_URL || 'postgres://localhost:5432/floorplanviz', function(err, client, done) {
+		client.query(queryString, function(err, result) {
+		    done();
+		    if(err) return console.error(err);
+
+		    for(var i in result.rows) {
+		    	console.log(result.rows[i]);
+		    	console.log(result.rows[i]['store_id']);
+		    	console.log(result.rows[i]['laststore_id']);
+		    	if (storePaths[result.rows[i]['laststore_id']] && 
+		    		storePaths[result.rows[i]['laststore_id']][result.rows[i]['store_id']]) {
+		    		storePaths[result.rows[i]['laststore_id']][result.rows[i]['store_id']]++;
+		    	} else {
+		    		if(!storePaths[result.rows[i]['laststore_id']])
+		    			storePaths[result.rows[i]['laststore_id']] = {};
+		    		storePaths[result.rows[i]['laststore_id']][result.rows[i]['store_id']] = 1;
+		    	}
+		    }
+		    console.log(storePaths);
 		    
-// 		  });
-// 	});
-// }
+		    for(var start in storePaths) {
+		    	console.log(start);
+		    	console.log(storePaths[start]);
+		    	for(var end in storePaths[start]) {
+		    		console.log(end);
+		    		console.log(storePaths[start][end]);
+		    		output.push({"start":start, "end":end, "value":storePaths[start][end]})
+		    	}
+		    }
+
+		    res.json({"paths":output});
+		});
+	});
+}
+
+
